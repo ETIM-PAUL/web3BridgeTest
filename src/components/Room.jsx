420af39249e97f542c27621d3574c8463babe4cb
etim <etimpaul22@gmail.com>
2023-07-04 12:47:31 +0100

    added responsiveness for cards

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -42,17 +42,17 @@ const Room = ({ title, type, allPersons, setAllPersons }) => {
 
   return (
     <div>
-      <div className="card w-fit bg-base-100 shadow-xl">
+      <div className="card w-fit sm:w-[300px] bg-base-100 shadow-xl">
         <div className="card-body mx-auto">
           <h2 className="card-title justify-center">{title}</h2>
           <div>
             <span className="font-bold text-sm text-start">Facilitator{type === "room" ? "" : "s"}</span>
-            <ul className="menu bg-base-200 w-fit rounded-box">
+            <ul className="menu bg-base-200 w-fit mx-auto rounded-box">
               {persons.length > 0 && persons.filter((person) => person.proficiency === "facilitator").map((person, index) => (
-                <li className="" key={index}><a className="hover:cursor-default">{person.fullName} - {returnRole(person.proficiency)}</a></li>
+                <li className="mx-auto" key={index}><a className="hover:cursor-default">{person.fullName} - {returnRole(person.proficiency)}</a></li>
               ))}
               {persons.filter((person) => person.proficiency === "facilitator").length === 0 &&
-                <span className="text-red-600">No Facilatator in this {type === "room" ? "room" : "conference room"}</span>
+                <span className="text-red-600">No Facilitators in this {type === "room" ? "room" : "conference room"}</span>
               }
             </ul>
           </div>
